10b6f9cda04cb1486ce6f200e625cc3324256a65
youjaebeom <dbwo4011@gmail.com>
2020-11-12 23:24:38 +0900

    ss

--- kittoscrap/spiders/metalbot.py
+++ kittoscrap/spiders/metalbot.py
@@ -8,6 +8,7 @@ import re
 from datetime import datetime, timedelta 
 import pymysql
 import re
+import urllib.request
 
 class MetalbotSpider(scrapy.Spider):
     name = 'metalbot'
@@ -16,16 +17,34 @@ class MetalbotSpider(scrapy.Spider):
 
     def parse(self, response):
         items=[]
+
         Platinum = response.xpath('//*[@id="PT-bid"]/text()').extract_first()
         Palladium = response.xpath('//*[@id="PD-bid"]/text()').extract_first()
         Rhodium = response.xpath('//*[@id="RH-bid"]/text()').extract_first()
         now = datetime.now()
         Time = now.strftime('%Y-%m-%d %H:%M')
+
+        # 환율 정보를 불러오는 라인
+        page = urllib.request.urlopen(
+            "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%ED%99%98%EC%9C%A8")
+        text = page.read().decode("utf8")
+        # 시간 정보를 확인 합니다
+        where = text.find('class="grp_info"> <em>')
+        start_of_time = where + 22
+        end_of_time = start_of_time + 16
+        prin = text[start_of_time:end_of_time]
+
+        # 달러
+        usdwhere = text.find('<span>미국 <em>USD</em></span></a></th> <td><span>')
+        usdletter = text[usdwhere + 48] + text[usdwhere + 50:usdwhere + 56]
+        usdletter = float(usdletter)
+
         metalitem = KittoscrapItem()
         metalitem['Time_now'] = Time
-        metalitem['Platinum'] = float(Platinum)
-        metalitem['Palladium'] = float(Palladium)
-        metalitem['Rhodium'] = float(Rhodium)
+        metalitem['Platinum'] = float(Platinum)*usdletter
+        metalitem['Palladium'] = float(Palladium)*usdletter
+        metalitem['Rhodium'] = float(Rhodium)*usdletter
+
         items.append(metalitem)
         print("Time => ", Time)
         print("Platinum => ", metalitem['Platinum'])
--- kittoscrap/start.py
+++ kittoscrap/start.py
@@ -15,12 +15,15 @@ def crawling_start():
     subprocess.call(['scrapy', 'crawl', 'metalbot'])
 
 # 환율 start
-def crawling_do():
+def crawling_pirce():
     subprocess.call(['python', 'usd.py'])
 
-schedule.every(1).minutes.do(crawling_start) #
-#schedule.every().day.at("10:30").do(crawling_do) #
-schedule.every(1).minutes.do(crawling_do) #30분마다 실행
+## 한국이 미국보다 14시간 빠름 14시에 크롤링 -> 시장 마지막 가격
+
+schedule.every(60).minutes.do(crawling_start) # 1시간마다
+schedule.every(60).minutes.do(crawling_pirce) # 1시간마다
+
+#schedule.every().day.at("09:00").do(crawling_pirce) #
 
 #실제 실행하게 하는 코드
 while True:
